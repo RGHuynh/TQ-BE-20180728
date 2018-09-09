3132d2304643f70173678c95d2902e218c459a28
RGHuynh <randall.g.huynh@gmail.com>
2018-09-09 05:46:28 -0400

    feat: log in authentication

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,10 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
+
 module TQ
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
